58645079dfa3ac34c4aa06544e0a7d5f4624ccf5
jsg3121 <xodm95@gmail.com>
2024-05-11 22:42:52 +0900

    fix(relation): 연관 포켓몬을 추가합니다

--- src/container/src/detail/detail.baseInfo/src/baseinfo.relationPokemon/src/RelationPokemon.component.tsx
+++ src/container/src/detail/detail.baseInfo/src/baseinfo.relationPokemon/src/RelationPokemon.component.tsx
@@ -12,18 +12,20 @@ const RelationPokemonComponent: FC<IFProps> = ({ name, evolutionId }) => {
   return (
     <Div>
       <h2>진화 체인</h2>
-      {evolutionId.map((id) => {
-        return (
-          <Image
-            key={`relation-pokemon-id-${id}`}
-            src={`${imageMode}/${id}.webp`}
-            width="10rem"
-            height="10rem"
-            alt={`포켓몬 ${name}`}
-            unoptimized
-          />
-        )
-      })}
+      <div className="relation-pokemon-list">
+        {evolutionId.map((id) => {
+          return (
+            <Image
+              key={`relation-pokemon-id-${id}`}
+              src={`${imageMode}/${id}.webp`}
+              width="10rem"
+              height="10rem"
+              alt={`포켓몬 ${name}`}
+              unoptimized
+            />
+          )
+        })}
+      </div>
     </Div>
   )
 }
@@ -31,10 +33,28 @@ const RelationPokemonComponent: FC<IFProps> = ({ name, evolutionId }) => {
 export default RelationPokemonComponent
 
 const Div = styled.div`
-  width: 100%;
-  height: 10rem;
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  margin-top: 2rem;
+  width: 24rem;
+  height: 16rem;
+  background-color: var(--color-primary-4);
+  border: 3px solid var(--color-primary-1);
+  border-radius: 1rem;
+  outline: 3px solid var(--color-primary-4);
+  padding: 1rem;
+  margin: 2rem auto 0;
+
+  & > h2 {
+    width: 100%;
+    height: 2rem;
+    font-size: 1.5rem;
+    line-height: 2rem;
+    border-bottom: 1px solid var(--color-primary-1);
+  }
+
+  & > .relation-pokemon-list {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 1rem;
+    padding: 1rem 0;
+  }
 `
