dae5916eb8bd511ad15a49c059aa5a91fb71ac03
jsg3121 <xodm95@gmail.com>
2024-07-05 20:48:24 +0900

    fix(buildMode): 빌드 모드 수정

--- src/common/src/buildMode.ts
+++ src/common/src/buildMode.ts
@@ -1,9 +1,9 @@
 export const GqlMode =
   process.env.NODE_ENV === 'production'
-    ? 'http://127.0.0.1:4000/graphql'
+    ? 'http://43.202.59.139:4000/graphql'
     : 'http://localhost:4000/graphql'
 
 export const imageMode =
   process.env.NODE_ENV === 'production'
-    ? 'http://127.0.0.1:4000/image'
+    ? 'http://43.202.59.139:4000/image'
     : 'http://localhost:4000/image'
