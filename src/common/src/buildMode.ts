cf4c3f59eb710d7e63af291959694fcaeb440e1b
jsg3121 <xodm95@gmail.com>
2024-07-06 01:33:41 +0900

    fix: build mode 수정

--- src/common/src/buildMode.ts
+++ src/common/src/buildMode.ts
@@ -1,9 +1,3 @@
-export const GqlMode =
-  process.env.NODE_ENV === 'production'
-    ? 'http://127.0.0.1:4000/graphql'
-    : 'http://localhost:4000/graphql'
+export const GqlMode = 'http://127.0.0.1:4000/graphql'
 
-export const imageMode =
-  process.env.NODE_ENV === 'production'
-    ? 'http://localhost:4000/image'
-    : 'http://localhost:4000/image'
+export const imageMode = 'http://localhost:4000/image'
