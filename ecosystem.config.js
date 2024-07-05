08139907886bf5f182495a7e5ad29e5fb2a5902f
jsg3121 <xodm95@gmail.com>
2024-07-05 20:37:52 +0900

    fix(pm2): pm2 설정 파일 추가

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'poke-korea',
+      script: 'npm',
+      args: 'start',
+      instances: 1,
+      exec_mode: 'cluster',
+      env_production: {
+        PORT: 3000,
+      },
+    },
+  ],
+}
