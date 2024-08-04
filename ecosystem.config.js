15db1980a929f97c2dc49a9ce6bad3d4967a0828
jsg3121 <xodm95@gmail.com>
2024-08-04 20:55:09 +0900

    fix(instance): 인스턴스 수 조정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       cwd: './',
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
-      instances: 2,
+      instances: 1,
       exec_mode: 'cluster',
       env_production: {
         PORT: 80,
