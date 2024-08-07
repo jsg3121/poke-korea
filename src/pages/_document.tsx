c6268d5d0a88a893ce73183c809c22a26941e44b
jsg3121 <xodm95@gmail.com>
2024-08-07 23:15:50 +0900

    fix(html)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,15 +41,17 @@ export default class MyDocument extends Document {
                 async
                 src="https://www.googletagmanager.com/gtag/js?id=G-28P8TKSR5M"
               ></script>
-              <script>
-                {`
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
 
                   gtag('config', 'G-28P8TKSR5M');
-                `}
-              </script>
+                `,
+                }}
+              />
             </>
           )}
         </Head>
