eef653092931a1735787d00c19d990ce4d6f51c6
장선규 (SunGyu Jang) <xodm95@gmail.com>
2024-10-14 10:54:59 +0900

    사이트맵 priority를 변경

--- src/pages/server-sitemap-2.xml/index.tsx
+++ src/pages/server-sitemap-2.xml/index.tsx
@@ -55,7 +55,7 @@ export const getServerSideProps: GetServerSideProps = async (props) => {
         loc: `https://poke-korea.com/detail/${pokemon.number}?shinyMode=shiny`, // 페이지 경로
         lastmod: new Date().toISOString(), // 최근변경일자
         changefreq: 'daily', // 페이지 주소 변경 빈도 (검색엔진에 제공됨) - always, daily, hourly, monthly, never, weekly, yearly 중 택 1
-        priority: 1, // 페이지 주소 우선순위 (검색엔진에 제공됨, 우선순위가 높은 순서대로 크롤링함)
+        priority: 0.7, // 페이지 주소 우선순위 (검색엔진에 제공됨, 우선순위가 높은 순서대로 크롤링함)
       }
     },
   )
