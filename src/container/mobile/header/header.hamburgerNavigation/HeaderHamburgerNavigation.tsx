ba20917b742e7719910217f2b05512737a058faa
jsg3121 <xodm95@gmail.com>
2025-06-04 13:25:24 +0900

    🛠️ fix : 슬라이드 에니메이션 로직 추가

--- src/container/mobile/header/header.hamburgerNavigation/HeaderHamburgerNavigation.tsx
+++ src/container/mobile/header/header.hamburgerNavigation/HeaderHamburgerNavigation.tsx
@@ -1,16 +1,30 @@
 import Link from 'next/link'
 import { Fragment, useState } from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import GithubIcon from '~/assets/icons/github.svg'
 import GmailIcon from '~/assets/icons/gmail.svg'
 import { useBodyScrollLock } from '~/hook/useBodyScrollLock'
 
+type StyleProps = {
+  isAnimating: boolean
+  isOpenHamburger: boolean
+}
+
 const HeaderHamburgerNavigation = () => {
   const [isOpenHamburger, setIsOpenHamburger] = useState<boolean>(false)
+  const [isAnimating, setIsAnimating] = useState<boolean>(false)
   useBodyScrollLock(isOpenHamburger)
 
   const handleClickButton = () => {
-    setIsOpenHamburger((prev) => !prev)
+    if (isOpenHamburger) {
+      setIsAnimating(() => true) // 애니메이션 시작
+      setIsOpenHamburger(() => false)
+      setTimeout(() => {
+        setIsAnimating(() => false)
+      }, 300) // 애니메이션 시간에 맞춤
+    } else {
+      setIsOpenHamburger(() => true)
+    }
   }
 
   return (
@@ -18,7 +32,11 @@ const HeaderHamburgerNavigation = () => {
       <Button aria-label="햄버거 네비게이션 버튼" onClick={handleClickButton}>
         <i></i>
       </Button>
-      <Aside data-is-open={isOpenHamburger ? 'open' : 'close'}>
+      <Aside
+        isAnimating={isAnimating}
+        isOpenHamburger={isOpenHamburger}
+        data-is-open={isOpenHamburger ? 'open' : 'close'}
+      >
         <div>
           <nav>
             <ul>
@@ -74,82 +92,104 @@ const Button = styled.button`
     display: block;
   }
 `
-const Aside = styled.aside`
-  width: 100vw;
-  height: calc(100vh - 4rem);
-  position: absolute;
-  top: 4rem;
-  right: 0;
+const Aside = styled.aside<StyleProps>`
+  @keyframes slideIn {
+    0% {
+      right: -100%;
+    }
+    100% {
+      right: 0;
+    }
+  }
 
-  & > div {
-    width: 12rem;
-    height: calc(100% - 4rem);
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    position: fixed;
-    background-color: var(--color-primary-1);
+  @keyframes slideOut {
+    0% {
+      right: 0;
+      display: flex;
+    }
+    100% {
+      right: -100%;
+      display: none;
+    }
+  }
+
+  ${({ isAnimating, isOpenHamburger }) => css`
+    width: 100vw;
+    height: calc(100vh - 4rem);
+    position: absolute;
     top: 4rem;
-    right: -45vw;
-    z-index: 1000;
-    transition: right 0.25s;
+    right: 0;
+    ${isOpenHamburger || isAnimating ? 'display: block;' : 'display: none;'}
 
-    & > nav {
-      width: 100%;
-      margin-top: 1rem;
-      padding: 0 1.25rem;
+    & > div {
+      width: 12rem;
+      height: calc(100% - 4rem);
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      position: fixed;
+      background-color: var(--color-primary-1);
+      top: 4rem;
+      right: -12rem;
+      animation: 0.3s ease 0s 1 forwards
+        ${isOpenHamburger ? 'slideIn' : 'slideOut'};
+      z-index: 1000;
 
-      & > ul {
+      & > nav {
         width: 100%;
+        margin-top: 1rem;
+        padding: 0 1.25rem;
+
+        & > ul {
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          gap: 1rem;
+
+          & > li {
+            width: 100%;
+
+            & > a {
+              width: 100%;
+              height: 1.5rem;
+              font-size: 1rem;
+              line-height: calc(1.5rem + 2px);
+              color: var(--color-primary-4);
+            }
+          }
+        }
+      }
+
+      & > div {
+        width: 100%;
+        height: 3rem;
         display: flex;
-        flex-direction: column;
+        align-items: center;
+        justify-content: center;
         gap: 1rem;
 
-        & > li {
-          width: 100%;
-
-          & > a {
-            width: 100%;
-            height: 1.5rem;
-            font-size: 1rem;
-            line-height: calc(1.5rem + 2px);
-            color: var(--color-primary-4);
-          }
+        & > a {
+          width: 2rem;
+          height: 2rem;
+          font-size: 0;
         }
       }
     }
 
-    & > div {
-      width: 100%;
-      height: 3rem;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      gap: 1rem;
+    &[data-is-open='open'] {
+      display: block;
 
-      & > a {
-        width: 2rem;
-        height: 2rem;
-        font-size: 0;
+      &::before {
+        content: '';
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.45);
+        display: block;
+        position: absolute;
+        top: 0;
+        right: 0;
+        z-index: 900;
       }
     }
-  }
-
-  &[data-is-open='open'] {
-    & > div {
-      right: 0;
-    }
-
-    &::before {
-      content: '';
-      width: 100%;
-      height: 100%;
-      background-color: rgba(0, 0, 0, 0.45);
-      display: block;
-      position: absolute;
-      top: 0;
-      right: 0;
-      z-index: 900;
-    }
-  }
+  `}
 `
