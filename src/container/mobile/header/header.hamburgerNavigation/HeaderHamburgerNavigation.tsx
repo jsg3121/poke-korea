d7246e629d7baed43f4eb1ae462f16a9cfac5c0f
jsg3121 <xodm95@gmail.com>
2025-06-09 00:24:23 +0900

    🛠️ fix : useState 사용처 'use client' 적용

--- src/container/mobile/header/header.hamburgerNavigation/HeaderHamburgerNavigation.tsx
+++ src/container/mobile/header/header.hamburgerNavigation/HeaderHamburgerNavigation.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from 'next/link'
 import { Fragment, useState } from 'react'
 import GithubIcon from '~/assets/icons/github.svg'
--- src/views/desktop/main/Header/filter/filter.pokemonType/FilterPokemonType.component.tsx
+++ src/views/desktop/main/Header/filter/filter.pokemonType/FilterPokemonType.component.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useRouter } from 'next/router'
 import { ChangeEvent, useState } from 'react'
 import ImageComponent from '~/components/Image.component'
