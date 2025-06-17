7c028cdd13874cabd9457cef98f5a431303c47ca
jsg3121 <xodm95@gmail.com>
2025-06-17 22:41:59 +0900

    🛠️ fix : 광고 영역 수정

--- src/components/adSlot/DesktopListTopBanner.tsx
+++ src/components/adSlot/DesktopListTopBanner.tsx
@@ -7,7 +7,7 @@ const DesktopListTopBanner = () => {
   return (
     <div ref={slotRef} className="w-full max-w-[1280px] h-fit mt-8 mx-auto">
       <ins
-        className="adsbygoogle w-full max-h-[140px] block mx-auto text-center"
+        className="adsbygoogle w-full h-[140px] block mx-auto text-center"
         data-ad-client="ca-pub-6481622724376761"
         data-ad-slot="9835534510"
         data-ad-format="auto"
--- src/components/adSlot/MobileListTopBanner.tsx
+++ src/components/adSlot/MobileListTopBanner.tsx
@@ -7,7 +7,7 @@ const MobileListTopBanner = () => {
   return (
     <div ref={slotRef} className="w-full h-fit mt-8 mx-auto">
       <ins
-        className="adsbygoogle w-[calc(100%-3rem)] max-h-[90px] block mx-auto text-center"
+        className="adsbygoogle w-[calc(100%-3rem)] h-[90px] block mx-auto text-center"
         data-ad-client="ca-pub-6481622724376761"
         data-ad-slot="1410249585"
         data-ad-format="auto"
