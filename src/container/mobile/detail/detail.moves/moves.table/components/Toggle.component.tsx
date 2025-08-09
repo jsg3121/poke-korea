6c656274c1c3532246da1b673b734821268510ce
jsg3121 <xodm95@gmail.com>
2025-08-10 02:10:08 +0900

    🎨 fix: 클릭 이벤트 실시간 반영 적용

--- src/container/mobile/detail/detail.moves/moves.table/components/Toggle.component.tsx
+++ src/container/mobile/detail/detail.moves/moves.table/components/Toggle.component.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ChangeEvent } from 'react'
+import { ChangeEvent, useState } from 'react'
 
 interface ToggleButtonComponentProps {
   defaultChecked?: boolean
@@ -11,8 +11,10 @@ const ToggleButtonComponent = ({
   defaultChecked = true,
   onClickToggle,
 }: ToggleButtonComponentProps) => {
+  const [defaultValue, setDefaultValue] = useState<boolean>(defaultChecked)
   const handleToggle = (e: ChangeEvent<HTMLInputElement>) => {
     const checked = e.currentTarget.checked
+    setDefaultValue(() => checked)
     onClickToggle(checked ? 'LEVELUP' : 'MACHINE')
   }
 
@@ -31,7 +33,7 @@ const ToggleButtonComponent = ({
       <i className="absolute h-full w-full rounded-full bg-primary-2 peer-checked:bg-primary-3 " />
       <span className="absolute h-[1.5rem] w-[1.5rem] rounded-full bg-white transform peer-checked:translate-x-[4.25rem] translate-x-[0.25rem]" />
       <span className="text-sm font-bold absolute transform translate-x-[3.5rem] text-primary-4 peer-checked:translate-x-[0.75rem] peer-checked:text-primary-1 select-none">
-        {defaultChecked ? '레벨업' : '머신'}
+        {defaultValue ? '레벨업' : '머신'}
       </span>
     </label>
   )
