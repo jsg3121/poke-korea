ab55114399007aa464281a9e8becffd9eb08f05c
jsg3121 <xodm95@gmail.com>
2025-09-20 19:11:05 +0900

    🛠️ fix : 결과 문구 수정

--- src/module/quiz.module.ts
+++ src/module/quiz.module.ts
@@ -17,7 +17,7 @@ export const getQuizResultCopy = (score: number): QuizResultType => {
 
   if (score <= 16) {
     return {
-      headline: '감 잡았다! ⚡️',
+      headline: '좋았어 감 잡았다! ⚡️',
       medal: '🥈',
       subcopy: '벌써 절반 가까이. 만점까지 얼마 남지 않았어요!',
     }
@@ -25,14 +25,14 @@ export const getQuizResultCopy = (score: number): QuizResultType => {
 
   if (score <= 19) {
     return {
-      headline: '챔피언 눈앞! 👑',
+      headline: '곧 챔피언 눈앞! 👑',
       medal: '🥇',
       subcopy: '너무 아쉬워요...!! 한 번 더 도전 해보는건 어떨까요?',
     }
   }
 
   return {
-    headline: '포켓몬 마스터! 🌟',
+    headline: '당신은 포켓몬 마스터! 🌟',
     medal: '🎖️',
     subcopy: '완벽 그 자체. 당신이 진정한 포켓몬 마스터!',
   }
