03207a577f992b1d5612d6de9888fd4a05d86987
jsg3121 <xodm95@gmail.com>
2025-09-26 20:50:52 +0900

    🛠️ fix: 결과 페이지 성적 공통 컴포넌트 분리

--- src/views/desktop/quiz/abilityQuiz/abilityQuiz.result/AbilityQuizResult.tsx
+++ src/views/desktop/quiz/abilityQuiz/abilityQuiz.result/AbilityQuizResult.tsx
@@ -5,7 +5,8 @@ import CorrectIcon from '~/assets/icons/correct-icon.svg'
 import { QUIZ_ROUTES } from '~/constants/quiz.constants'
 import { useAbilityQuizContext } from '~/context/AbilityQuiz.context'
 import { getQuizResultCopy } from '~/module/quiz.module'
-import { formatTime } from '~/utils/quiz.util'
+import ResultHeader from '../../components/result/ResultHeader'
+import ResultSummary from '../../components/result/ResultSummary'
 
 const AbilityQuizResult = () => {
   const { result, questions, onClickRetryQuiz } = useAbilityQuizContext()
@@ -20,43 +21,13 @@ const AbilityQuizResult = () => {
 
   return (
     <section className="h-fit w-full max-w-[1280px] mx-auto pt-[3rem]">
-      <header className="w-full h-[22rem]">
-        <span className="w-fit h-[14rem] text-[10rem] block mx-auto">
-          {medal}
-        </span>
-        <h1 className="w-full text-[2rem] font-bold text-center leading-[calc(2rem+2px)] text-primary-4">
-          {headline}
-        </h1>
-        <p className="w-full h-[1.25rem] text-[1.25rem] text-center text-primary-3 leading-[calc(1.25rem+2px)] mt-[1.5rem]">
-          {subcopy}
-        </p>
-      </header>
-      <dl className="w-full h-[6.5rem] bg-primary-4 rounded-[2rem] p-[2rem] flex items-center justify-around mb-[2rem]">
-        <dt className="text-[1.25rem] font-[500] h-[2.5rem] leading-[calc(2.5rem+2px)] text-primary-1">
-          맞은 문제
-        </dt>
-        <dd className="text-[2.25rem] h-[2.5rem] leading-[calc(2.5rem+2px)] font-bold text-primary-1">
-          {result.correctAnswers} 개
-        </dd>
-        <dt className="text-[1.25rem] font-[500] h-[2.5rem] leading-[calc(2.5rem+2px)] text-primary-1">
-          정답률
-        </dt>
-        <dd className="text-[2.25rem] h-[2.5rem] leading-[calc(2.5rem+2px)] font-bold text-primary-1">
-          {result.percentage} %
-        </dd>
-        <dt className="text-[1.25rem] font-[500] h-[2.5rem] leading-[calc(2.5rem+2px)] text-primary-1">
-          소요 시간
-        </dt>
-        <dd className="text-[2.25rem] h-[2.5rem] leading-[calc(2.5rem+2px)] font-bold text-primary-1">
-          {formatTime(result.totalTime)}
-        </dd>
-        <dt className="text-[1.25rem] font-[500] h-[2.5rem] leading-[calc(2.5rem+2px)] text-primary-1">
-          평균 시간
-        </dt>
-        <dd className="text-[2.25rem] h-[2.5rem] leading-[calc(2.5rem+2px)] font-bold text-primary-1">
-          {formatTime(result.averageTime)}
-        </dd>
-      </dl>
+      <ResultHeader headline={headline} medal={medal} subcopy={subcopy} />
+      <ResultSummary
+        averageTime={result.averageTime}
+        correctAnswers={result.correctAnswers}
+        percentage={result.percentage}
+        totalTime={result.totalTime}
+      />
       <article className="w-full h-fit bg-primary-4 rounded-[2rem] py-[1rem] px-[2rem] mb-[2rem]">
         <h2 className="w-full h-[3rem] text-primary-1 font-bold leading-[calc(2rem+2px)] text-[1.25rem]">
           문제 정답
---
+++ src/views/desktop/quiz/components/result/ResultSummary.tsx
@@ -0,0 +1,46 @@
+import { formatTime } from '~/utils/quiz.util'
+
+interface ResultSummaryProps {
+  correctAnswers: number
+  percentage: number
+  totalTime: number
+  averageTime: number
+}
+
+const ResultSummary = ({
+  averageTime,
+  correctAnswers,
+  percentage,
+  totalTime,
+}: ResultSummaryProps) => {
+  return (
+    <dl className="w-full h-[6.5rem] bg-primary-4 rounded-[2rem] p-[2rem] flex items-center justify-around mb-[2rem]">
+      <dt className="text-[1.25rem] font-[500] h-[2.5rem] leading-[calc(2.5rem+2px)] text-primary-1">
+        맞은 문제
+      </dt>
+      <dd className="text-[2.25rem] h-[2.5rem] leading-[calc(2.5rem+2px)] font-bold text-primary-1">
+        {correctAnswers} 개
+      </dd>
+      <dt className="text-[1.25rem] font-[500] h-[2.5rem] leading-[calc(2.5rem+2px)] text-primary-1">
+        정답률
+      </dt>
+      <dd className="text-[2.25rem] h-[2.5rem] leading-[calc(2.5rem+2px)] font-bold text-primary-1">
+        {percentage} %
+      </dd>
+      <dt className="text-[1.25rem] font-[500] h-[2.5rem] leading-[calc(2.5rem+2px)] text-primary-1">
+        소요 시간
+      </dt>
+      <dd className="text-[2.25rem] h-[2.5rem] leading-[calc(2.5rem+2px)] font-bold text-primary-1">
+        {formatTime(totalTime)}
+      </dd>
+      <dt className="text-[1.25rem] font-[500] h-[2.5rem] leading-[calc(2.5rem+2px)] text-primary-1">
+        평균 시간
+      </dt>
+      <dd className="text-[2.25rem] h-[2.5rem] leading-[calc(2.5rem+2px)] font-bold text-primary-1">
+        {formatTime(averageTime)}
+      </dd>
+    </dl>
+  )
+}
+
+export default ResultSummary
