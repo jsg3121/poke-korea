e9ef209b8c9aa2025146c0a04cd737aab913a0b7
jsg3121 <xodm95@gmail.com>
2025-09-29 11:55:35 +0900

    🛠️ fix : 검색결과가 없는 경우 문구 추가

--- src/container/desktop/List/List.container.tsx
+++ src/container/desktop/List/List.container.tsx
@@ -34,16 +34,28 @@ const ListContainer = () => {
   return (
     <section className="w-full max-w-[1280px] min-h-dvh h-full mx-auto py-12 pb-8 relative [&>h2]:absolute [&>h2]:top-0 [&>h2]:text-primary-1 [&>h2]:select-none">
       <h2 className="visually-hidden">포켓몬 리스트</h2>
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(calc(14rem-10px),auto))] gap-x-4 gap-y-6 justify-items-center justify-between px-5">
-        {pokemonList.map((pokemon) => {
-          return (
-            <PokemonCardComponent
-              key={`pokemon-id-${pokemon.id}`}
-              pokemonData={pokemon}
-            />
-          )
-        })}
-      </div>
+      {pokemonList.length === 0 && (
+        <div className="w-full h-[20rem]">
+          <p className="w-full text-[2rem] text-primary-4 font-bold text-center">
+            검색 결과에 맞는 포켓몬이 없어요!
+          </p>
+          <p className="w-full text-[2rem] text-primary-4 font-bold text-center">
+            포켓몬 이름을 다시 확인해주세요!
+          </p>
+        </div>
+      )}
+      {pokemonList.length > 0 && (
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(calc(14rem-10px),auto))] gap-x-4 gap-y-6 justify-items-center justify-between px-5">
+          {pokemonList.map((pokemon) => {
+            return (
+              <PokemonCardComponent
+                key={`pokemon-id-${pokemon.id}`}
+                pokemonData={pokemon}
+              />
+            )
+          })}
+        </div>
+      )}
       {isLoadingMore && (
         <div className="flex justify-center py-4 w-full col-span-full">
           <div className="text-sm text-gray-600">
