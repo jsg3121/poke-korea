7862d0a9440e3a16ec918485ed4513f651292e3b
jsg3121 <xodm95@gmail.com>
2025-10-01 22:51:15 +0900

    🛠️ hotfix : 퀴즈 메인 중앙 정렬 추가 수정

--- src/components/adSlot/DesktopQuizMainTopBanner.tsx
+++ src/components/adSlot/DesktopQuizMainTopBanner.tsx
@@ -6,7 +6,7 @@ const DesktopQuizMainTopBanner = () => {
   return (
     <div ref={slotRef} className="w-full h-fit max-w-[1280px] col-span-full">
       <ins
-        className="adsbygoogle w-full h-[90px] block text-center mt-8"
+        className="adsbygoogle w-full h-[90px] block text-center mt-8 mx-auto"
         data-ad-client="ca-pub-6481622724376761"
         data-ad-slot="7044940678"
       ></ins>
