3dadaa04265a622ca7ae838f98c9186c38db201f
jsg3121 <xodm95@gmail.com>
2025-10-01 22:53:32 +0900

    🛠️ hotfix :  모바일 상세 기술 광고 배너 사이즈 조정

--- src/components/adSlot/MobileDetailMovesBanner.tsx
+++ src/components/adSlot/MobileDetailMovesBanner.tsx
@@ -7,10 +7,10 @@ const MobileDetailMovesBanner = () => {
   return (
     <div
       ref={slotRef}
-      className="w-[calc(100%-3rem)] h-fit text-center mx-auto"
+      className="w-[calc(100%-3rem)] h-[100px] text-center mx-auto"
     >
       <ins
-        className="adsbygoogle w-[300px] h-[250px] block text-center mx-auto"
+        className="adsbygoogle w-[320px] h-[100px] block text-center mx-auto"
         data-ad-client="ca-pub-6481622724376761"
         data-ad-slot="3916466104"
       ></ins>
