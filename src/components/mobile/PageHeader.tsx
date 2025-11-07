9efb50947f4d6f2b94d9cfa74b158df41c98b720
jsg3121 <xodm95@gmail.com>
2025-11-07 19:21:05 +0900

    🛠️ fix : 기술도감 타이틀 수정

--- src/components/mobile/PageHeader.tsx
+++ src/components/mobile/PageHeader.tsx
@@ -5,11 +5,11 @@ interface PageHeaderProps {
 
 const PageHeader = ({ description, title }: PageHeaderProps) => {
   return (
-    <header className="w-full h-30 text-center border-b border-solid border-primary-4 pt-4">
+    <header className="w-full h-30 text-center border-b border-solid border-primary-4 pt-4 mb-6">
       <h1 className="h-16 text-[2rem] text-center leading-[4rem] text-primary-4 font-bold">
         {title}
       </h1>
-      <p className="text-[0.875rem] text-primary-3 max-w-2xl mx-auto">
+      <p className="text-[0.875rem] text-primary-3 max-w-2xl mx-auto wrap whitespace-break-spaces ">
         {description}
       </p>
     </header>
--- src/views/mobile/moves/Moves.mobile.tsx
+++ src/views/mobile/moves/Moves.mobile.tsx
@@ -1,4 +1,5 @@
 'use client'
+import PageHeader from '~/components/mobile/PageHeader'
 import MobileTabBar from '~/components/MobileTabBar'
 import HeaderContainer from '~/container/mobile/header/Header.container'
 import MovesListContainer from '~/container/mobile/moves/MovesList.container'
@@ -8,16 +9,10 @@ const MovesMobile = () => {
     <main>
       <HeaderContainer />
       <section className="w-full">
-        <header className="w-full h-24 text-center border-b border-solid border-primary-4 pb-4">
-          <h1 className="h-12 text-[2rem] text-center leading-[3rem] text-primary-4 font-[700]">
-            포켓몬 기술 도감
-          </h1>
-          <p className="w-full h-8 text-center text-[0.8rem] text-primary-3">
-            포켓몬이 사용할 수 있는 모든 기술을 한눈에 확인하세요.
-            <br />
-            타입, 위력, PP, 설명을 확인하고 검색할 수 있습니다.
-          </p>
-        </header>
+        <PageHeader
+          title="포켓몬 기술 도감"
+          description={`포켓몬이 사용할 수 있는 모든 기술을 한눈에 확인하세요.\n타입, 위력, PP, 설명을 확인하고 검색할 수 있습니다.`}
+        />
         <MovesListContainer />
       </section>
       <MobileTabBar />
