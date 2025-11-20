e198e883e9f8b127e853413a6fb82bcbf772bf63
jsg3121 <xodm95@gmail.com>
2025-11-20 22:12:36 +0900

    🛠️ feat: 모바일 탭바 홈 링크 추가

--- src/components/MobileTabBar.tsx
+++ src/components/MobileTabBar.tsx
@@ -6,6 +6,7 @@ import QuizIcon from '~/assets/icons/quiz.svg'
 import MovesListIcon from '~/assets/icons/movesList.svg'
 import TypeEffectivenessIcon from '~/assets/icons/typeEffectiveness.svg'
 import AbilityIcon from '~/assets/icons/ability.svg'
+import PokeballIcon from '~/assets/icons/pokeball.svg'
 import { usePathname } from 'next/navigation'
 
 const MobileTabBar = () => {
@@ -13,27 +14,26 @@ const MobileTabBar = () => {
 
   return (
     <nav className="w-full h-20 bg-primary-1 border-t border-solid border-primary-2 fixed bottom-0 left-0 z-50">
-      <ul className="w-full h-full grid grid-cols-5">
+      <ul className="w-full h-full grid grid-cols-6">
+        <li
+          className={`w-full h-full text-center ${pathname === '/' ? 'border-t-4 border-solid border-primary-4' : ''}`}
+        >
+          <Link
+            href="/"
+            className={`w-full h-full flex flex-col justify-end gap-2 items-center text-[10px] leading-[calc(1.5rem+2px)] text-primary-4 [&>svg]:h-8 ${pathname === '/' ? 'font-bold' : ''}`}
+          >
+            <TabHomeIcon />홈
+          </Link>
+        </li>
         <li
           className={`w-full h-full text-center ${pathname === '/list' || pathname.includes('/detail') ? 'border-t-4 border-solid border-primary-4' : ''}`}
         >
           <Link
             href="/list"
-            className={`w-full h-full flex flex-col justify-end gap-2 items-center text-base leading-[calc(1.5rem+2px)] text-primary-4 [&>svg]:h-8 ${pathname === '/list' || pathname.includes('/detail') ? 'font-bold' : ''}`}
+            className={`w-full h-full flex flex-col justify-end gap-2 items-center text-[10px] leading-[calc(1.5rem+2px)] text-primary-4 [&>svg]:h-8 ${pathname === '/list' || pathname.includes('/detail') ? 'font-bold' : ''}`}
           >
-            <TabHomeIcon />
+            <PokeballIcon />
             포켓몬 도감
-          </Link>
-        </li>
-        <li
-          className={`w-full h-full text-center ${pathname === '/type-effectiveness' ? 'border-t-4 border-solid border-primary-4' : ''}`}
-        >
-          <Link
-            href="/type-effectiveness"
-            className={`w-full h-full flex flex-col justify-end gap-2 items-center text-base leading-[calc(1.5rem+2px)] text-primary-4 [&>svg]:h-8 ${pathname === '/type-effectiveness' ? 'font-bold' : ''}`}
-          >
-            <TypeEffectivenessIcon />
-            상성 계산기
           </Link>
         </li>
         <li
@@ -41,7 +41,7 @@ const MobileTabBar = () => {
         >
           <Link
             href="/moves"
-            className={`w-full h-full flex flex-col justify-end gap-2 items-center text-base leading-[calc(1.5rem+2px)] text-primary-4 [&>svg]:h-8 ${pathname === '/moves' ? 'font-bold' : ''}`}
+            className={`w-full h-full flex flex-col justify-end gap-2 items-center text-[10px] leading-[calc(1.5rem+2px)] text-primary-4 [&>svg]:h-8 ${pathname === '/moves' ? 'font-bold' : ''}`}
           >
             <MovesListIcon />
             기술 도감
@@ -52,10 +52,21 @@ const MobileTabBar = () => {
         >
           <Link
             href="/ability"
-            className={`w-full h-full flex flex-col justify-end gap-2 items-center text-base leading-[calc(1.5rem+2px)] text-primary-4 [&>svg]:h-8 ${pathname.includes('/ability') ? 'font-bold' : ''}`}
+            className={`w-full h-full flex flex-col justify-end gap-2 items-center text-[10px] leading-[calc(1.5rem+2px)] text-primary-4 [&>svg]:h-8 ${pathname.includes('/ability') ? 'font-bold' : ''}`}
           >
             <AbilityIcon />
             특성 도감
+          </Link>
+        </li>
+        <li
+          className={`w-full h-full text-center ${pathname === '/type-effectiveness' ? 'border-t-4 border-solid border-primary-4' : ''}`}
+        >
+          <Link
+            href="/type-effectiveness"
+            className={`w-full h-full flex flex-col justify-end gap-2 items-center text-[10px] leading-[calc(1.5rem+2px)] text-primary-4 [&>svg]:h-8 ${pathname === '/type-effectiveness' ? 'font-bold' : ''}`}
+          >
+            <TypeEffectivenessIcon />
+            상성 계산기
           </Link>
         </li>
         <li
@@ -63,7 +74,7 @@ const MobileTabBar = () => {
         >
           <Link
             href="/quiz"
-            className={`w-full h-full flex flex-col justify-end gap-2 items-center text-base leading-[calc(1.5rem+2px)] text-primary-4 [&>svg]:h-8 ${pathname.includes('/quiz') ? 'font-bold' : ''}`}
+            className={`w-full h-full flex flex-col justify-end gap-2 items-center text-[10px] leading-[calc(1.5rem+2px)] text-primary-4 [&>svg]:h-8 ${pathname.includes('/quiz') ? 'font-bold' : ''}`}
           >
             <QuizIcon />
             포켓몬 퀴즈
