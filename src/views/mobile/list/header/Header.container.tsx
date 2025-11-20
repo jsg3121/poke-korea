0e2abad1aa804eec9a37c7dea0e110cf1b5db4e6
jsg3121 <xodm95@gmail.com>
2025-11-20 22:34:47 +0900

    🛠️ fix : list 페이지 로고 제거

--- src/views/mobile/list/header/Header.container.tsx
+++ src/views/mobile/list/header/Header.container.tsx
@@ -1,11 +1,9 @@
-import LogoComponent from './logo/Logo.component'
 import SearchComponent from './search/Search.component'
 import FilterComponents from './filter/Filter.components'
 
 const HeaderContainer = () => {
   return (
-    <header className="w-full min-h-60 pt-12">
-      <LogoComponent />
+    <header className="w-full min-h-60">
       <SearchComponent />
       <FilterComponents />
     </header>
