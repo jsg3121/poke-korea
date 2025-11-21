732cc433355c414217e11981b46f5c723cc83077
jsg3121 <xodm95@gmail.com>
2025-11-22 00:57:12 +0900

    🛠️ fix : 네이티브 광고 테스트 더 나은 광고로 반영

--- src/components/adSlot/DesktopListTopBanner.tsx
+++ src/components/adSlot/DesktopListTopBanner.tsx
@@ -6,11 +6,6 @@ const DesktopListTopBanner = () => {
 
   return (
     <div ref={slotRef} className="w-full max-w-[1280px] h-fit mt-8 mx-auto">
-      {/* <ins
-        className="adsbygoogle w-[728px] h-[90px] block mx-auto text-center"
-        data-ad-client="ca-pub-6481622724376761"
-        data-ad-slot="9835534510"
-      ></ins> */}
       <ins
         className="adsbygoogle block mx-auto text-center"
         data-ad-format="fluid"
--- src/components/adSlot/MobileTypeEffectivenessBanner.tsx
+++ src/components/adSlot/MobileTypeEffectivenessBanner.tsx
@@ -6,17 +6,10 @@ const MobileTypeEffectivenessBanner = () => {
 
   return (
     <div ref={slotRef} className="w-full h-fit mx-auto">
-      {/* <ins
+      <ins
         className="adsbygoogle w-[320px] h-[100px] block mx-auto"
         data-ad-client="ca-pub-6481622724376761"
         data-ad-slot="1180520963"
-      ></ins> */}
-      <ins
-        className="adsbygoogle block"
-        data-ad-format="fluid"
-        data-ad-layout-key="-fu+52+56-cp+49"
-        data-ad-client="ca-pub-6481622724376761"
-        data-ad-slot="2341482094"
       ></ins>
     </div>
   )
