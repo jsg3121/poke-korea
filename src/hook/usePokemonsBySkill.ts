65bf3eeb7650e3ef482b2ff2f5a1e578e9755602
jsg3121 <xodm95@gmail.com>
2025-12-03 23:01:49 +0900

    🛠️ feat: 기술 상세 페이지 Hook 생성

---
+++ src/hook/usePokemonsBySkill.ts
@@ -0,0 +1,77 @@
+'use client'
+
+import { useGetPokemonsBySkillQuery } from '~/graphql/gqlGenerated'
+import { LearnMethod, PokemonLearnInfo } from '~/graphql/typeGenerated'
+import {
+  mergePagedResults,
+  extractNodesFromEdges,
+} from '~/module/graphqlPagination.module'
+
+interface UsePokemonsBySkillProps {
+  skillId: number
+  method?: LearnMethod
+  versionGroupId?: number
+  initialPokemonList?: Array<PokemonLearnInfo>
+  pageSize?: number
+}
+
+export const usePokemonsBySkill = ({
+  skillId,
+  method,
+  versionGroupId,
+  initialPokemonList = [],
+  pageSize = 30,
+}: UsePokemonsBySkillProps) => {
+  const { data, loading, fetchMore, error } = useGetPokemonsBySkillQuery({
+    variables: {
+      input: {
+        filter: {
+          skillId,
+          method,
+          versionGroupId,
+        },
+        pagination: {
+          first: pageSize,
+        },
+      },
+    },
+    skip: !skillId,
+  })
+
+  const loadMore = async () => {
+    if (!data?.getPokemonsBySkill?.pageInfo.hasNextPage) return
+
+    await fetchMore({
+      variables: {
+        input: {
+          filter: {
+            skillId,
+            method,
+            versionGroupId,
+          },
+          pagination: {
+            first: pageSize,
+            after: data?.getPokemonsBySkill.pageInfo.endCursor,
+          },
+        },
+      },
+      updateQuery: (prev, { fetchMoreResult }) =>
+        mergePagedResults('getPokemonsBySkill', prev, fetchMoreResult),
+    })
+  }
+
+  const pokemonList = extractNodesFromEdges(
+    data?.getPokemonsBySkill?.edges,
+    initialPokemonList,
+  )
+
+  return {
+    skillName: data?.getPokemonsBySkill?.skillName,
+    pokemonList,
+    hasNextPage: data?.getPokemonsBySkill?.pageInfo.hasNextPage,
+    loading,
+    error,
+    totalCount: data?.getPokemonsBySkill?.totalCount || 0,
+    loadMore,
+  }
+}
