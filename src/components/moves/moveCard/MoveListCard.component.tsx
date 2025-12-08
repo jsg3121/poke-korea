d592ea1428620300906392d817f69905797f3d0d
jsg3121 <xodm95@gmail.com>
2025-12-08 21:28:59 +0900

    🎨 fix : 기술 카드 스타일 수정

--- src/components/moves/moveCard/MoveListCard.component.tsx
+++ src/components/moves/moveCard/MoveListCard.component.tsx
@@ -17,9 +17,12 @@ const MoveCard = ({ moveData }: MoveCardProps) => {
       className="block w-full"
       aria-label={`${moveData.nameKo} 기술 상세보기`}
     >
-      <article className="w-full min-h-[180px] bg-primary-4 border-[2px] border-solid border-primary-1 rounded-xl shadow-[0_0_0px_3px_var(--color-primary-4)] p-4 relative hover:scale-105 transition-transform">
+      <article className="w-full min-h-[160px] bg-primary-4 border-[2px] border-solid border-primary-1 rounded-xl shadow-[0_0_0px_3px_var(--color-primary-4)] p-4 relative hover:scale-105 transition-transform">
         <header className="w-full h-8 mb-3 flex items-center justify-between">
           <h3 className="h-6 text-[1.25rem] leading-[calc(1.5rem+2px)] font-bold text-gray-900 flex gap-2">
+            <span className="h-4 text-[1rem] text-primary-2">
+              {moveData.id}.
+            </span>
             {moveData.nameKo}
             {moveData.type && (
               <span
@@ -41,23 +44,28 @@ const MoveCard = ({ moveData }: MoveCardProps) => {
             {damageType}
           </span>
         </header>
-        <dl className="grid grid-cols-3 gap-3 mb-3">
-          <dt className="text-[0.75rem] text-gray-600 mb-1">위력</dt>
-          <dd className="text-[1rem] font-bold text-primary-1">
-            {moveData.power || '-'}
-          </dd>
-          <dt className="text-[0.75rem] text-gray-600 mb-1">명중률</dt>
-          <dd className="text-[1rem] font-bold text-primary-1">
-            {moveData.accuracy || '-'}
-          </dd>
-          <dt className="text-[0.75rem] text-gray-600 mb-1">PP</dt>
-          <dd className="text-[1rem] font-bold text-primary-1">
-            {moveData.pp || '-'}
-          </dd>
+        <dl className="grid grid-cols-3 mb-3">
+          <div className="w-full h-full text-center relative after:absolute after:-right-0 after:bg-primary-3 after:top-0 after:h-full after:w-[2px]">
+            <dt className="text-[0.75rem] text-gray-600 mb-1">위력</dt>
+            <dd className="text-[1rem] font-bold text-primary-1">
+              {moveData.power || '-'}
+            </dd>
+          </div>
+          <div className="w-full h-full text-center relative after:absolute after:-right-0 after:bg-primary-3 after:top-0 after:h-full after:w-[2px]">
+            <dt className="text-[0.75rem] text-gray-600 mb-1">명중률</dt>
+            <dd className="text-[1rem] font-bold text-primary-1">
+              {moveData.accuracy || '-'}
+            </dd>
+          </div>
+          <div className="w-full h-full text-center">
+            <dt className="text-[0.75rem] text-gray-600 mb-1">PP</dt>
+            <dd className="text-[1rem] font-bold text-primary-1">
+              {moveData.pp || '-'}
+            </dd>
+          </div>
         </dl>
-
         <p className="text-[0.875rem] text-primary-2 font-semibold absolute bottom-4 left-4">
-          자세히 보기 &gt;
+          세대별 기술 정보 보러 가기 &gt;
         </p>
       </article>
     </Link>
