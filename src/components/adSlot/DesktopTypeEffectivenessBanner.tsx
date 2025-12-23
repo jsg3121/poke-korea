61b783bbf460d770caec165bba700eece214c964
jsg3121 <xodm95@gmail.com>
2025-12-23 23:12:45 +0900

    🛠️ fix : 광고 슬롯 사이즈 변경

--- src/components/adSlot/DesktopTypeEffectivenessBanner.tsx
+++ src/components/adSlot/DesktopTypeEffectivenessBanner.tsx
@@ -5,11 +5,6 @@ const DesktopTypeEffectivenessBanner = () => {
   const { slotRef } = useAdSlotEffect()
   return (
     <div ref={slotRef} className="w-full max-w-[1280px] h-fit">
-      {/* <ins
-        className="adsbygoogle w-[970px] h-[250px] block mx-auto"
-        data-ad-client="ca-pub-6481622724376761"
-        data-ad-slot="6041965400"
-      ></ins> */}
       <ins
         className="adsbygoogle block text-center mx-auto"
         data-ad-layout="in-article"
