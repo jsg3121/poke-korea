5b686cbe77cb9f59598528fd7174b8c4607c8287
jsg3121 <xodm95@gmail.com>
2025-12-23 22:47:17 +0900

    🛠️ fix : PC 포켓몬 상세 사이드배너 1280px 미만 미노출일때 광고 요청 로직 비활성화로 수정

--- src/components/adSlot/DesktopDetailSidebarBanner.tsx
+++ src/components/adSlot/DesktopDetailSidebarBanner.tsx
@@ -1,19 +1,41 @@
 'use client'
-import { useAdSlotEffect } from '~/hook/useAdSlotEffect'
+
+import { useEffect, useRef } from 'react'
 
 const DesktopDetailSidebarBanner = () => {
-  const { slotRef } = useAdSlotEffect()
+  const slotRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (window.innerWidth < 1280) {
+      return
+    } else {
+      const bannerElement =
+        slotRef.current?.querySelector<HTMLDivElement>('.adsbygoogle')
+
+      if (
+        typeof window !== 'undefined' &&
+        bannerElement &&
+        bannerElement.getAttribute('data-adsbygoogle-status') !== 'done'
+      ) {
+        try {
+          ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+        } catch (e) {
+          console.error('AdSense push error:', e)
+        }
+      }
+    }
+  }, [])
+
   return (
     <div
       ref={slotRef}
       className="max-w-40 max-h-fit absolute -right-[200px] xl:static xl:right-0 max-xl:w-0 max-xl:hidden"
     >
       <ins
-        className="adsbygoogle w-40 max-h-[600px] block ml-4"
+        className="adsbygoogle w-40 h-[600px] block ml-4"
         data-ad-client="ca-pub-6481622724376761"
         data-ad-slot="9075172521"
         data-ad-format="auto"
-        data-full-width-responsive="true"
       ></ins>
     </div>
   )
