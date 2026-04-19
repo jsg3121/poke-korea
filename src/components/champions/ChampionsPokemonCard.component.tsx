054fc4a804945fad500b41aba8240b8d92d7bd2d
jsg3121 <xodm95@gmail.com>
2026-04-19 17:13:32 +0900

    🎨 fix : 이미지 최적화

--- src/components/champions/ChampionsPokemonCard.component.tsx
+++ src/components/champions/ChampionsPokemonCard.component.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link'
 import BallComponent from '~/components/Ball.component'
 import TagComponent from '~/components/Tag.component'
+import { useDevice } from '~/context/Device.context'
 import { ChampionsPokemonCardFragment } from '~/graphql/typeGenerated'
 import { useLazyImage } from '~/hook/useLazyImage'
 import { imageMode } from '~/module/buildMode'
@@ -10,6 +11,7 @@ import {
   getBackgroundColor,
   pokemonNumberFormat,
 } from '~/module/pokemonCard.module'
+import ImageComponent from '../Image.component'
 
 interface ChampionsPokemonCardProps {
   pokemonData: ChampionsPokemonCardFragment
@@ -20,11 +22,15 @@ const ChampionsPokemonCard = ({
   pokemonData,
   isHighPriority = false,
 }: ChampionsPokemonCardProps) => {
+  const { isMobile } = useDevice()
   const pokemonNumber = pokemonNumberFormat(pokemonData.pokemonNumber)
   const backgroundColor = getBackgroundColor(pokemonData.types)
-  const displayName = pokemonData.formName
-    ? `${pokemonData.name} (${pokemonData.formName})`
-    : pokemonData.name
+
+  const getDisplayName = () => {
+    const suffix = pokemonData.region || pokemonData.formName
+    return suffix ? `${pokemonData.name} (${suffix})` : pokemonData.name
+  }
+  const displayName = getDisplayName()
 
   const { imgRef, isVisible, isLoaded, handleImageLoad, handleImageError } =
     useLazyImage({
@@ -45,7 +51,7 @@ const ChampionsPokemonCard = ({
       className="w-full max-w-56"
     >
       <article
-        className="w-full max-w-56 h-80 text-black-2 border border-solid border-black-2 rounded-[10px] block p-[0.83333333rem_0.55555556rem] outline-[0.25rem] outline outline-white relative overflow-hidden shadow-[inset_10px_0_0_0_rgb(51_65_80)] cursor-pointer transition-transform duration-300 ease-[cubic-bezier(0.03,0.57,0.37,1.02)] hover:scale-[1.2] hover:z-10 card-corner-fold"
+        className="w-full md:max-w-56 h-80 text-black-2 border border-solid border-black-2 rounded-[10px] block p-[0.75rem_0.5rem] outline-[0.25rem] outline outline-white relative overflow-hidden shadow-[inset_10px_0_0_0_rgb(51_65_80)] cursor-pointer transition-transform duration-300 ease-[cubic-bezier(0.03,0.57,0.37,1.02)] hover:scale-[1.2] hover:z-10 card-corner-fold"
         style={gradientStyle}
         aria-label={`포켓몬 ${displayName} 카드`}
       >
@@ -53,11 +59,13 @@ const ChampionsPokemonCard = ({
           <i className="w-8 h-8 flex-shrink-0 mr-2">
             <BallComponent />
           </i>
-          <div className="w-full h-5 flex items-start justify-between border-b border-solid border-card-accent pb-1">
+          <div className="w-full min-h-5 flex items-start flex-wrap justify-between border-b border-solid border-card-accent pb-1">
             <p className="h-4 text-base leading-none font-medium text-black-2">
               No.{pokemonNumber}
             </p>
-            <h3 className="w-full h-4 text-base leading-none font-semibold text-right text-black truncate">
+            <h3
+              className={`w-fit h-4 leading-none font-semibold text-right text-black ${displayName && displayName.length > 9 ? 'text-[0.75rem]' : 'text-base'}`}
+            >
               {displayName}
             </h3>
           </div>
@@ -65,12 +73,18 @@ const ChampionsPokemonCard = ({
         {isHighPriority ? (
           <div className="w-fit mx-auto mb-2 drop-shadow-[2px_3px_2px_#333333] relative">
             {pokemonData.imagePath && (
-              <img
+              <ImageComponent
+                height={isMobile ? '8rem' : '10rem'}
+                width={isMobile ? '8rem' : '10rem'}
+                imageSize={{
+                  width: isMobile ? 96 : 160,
+                  height: isMobile ? 96 : 160,
+                }}
+                densities={[1, 1.5]}
+                alt={`pokemon_id_${pokemonData.pokemonNumber} ${pokemonData.name}`}
                 src={`${imageMode}/${pokemonData.imagePath}.webp`}
-                alt={displayName}
-                width={160}
-                height={160}
-                className="w-40 h-40 object-contain"
+                sizes={isMobile ? '8rem' : '10rem'}
+                fetchPriority="high"
               />
             )}
           </div>
@@ -82,12 +96,17 @@ const ChampionsPokemonCard = ({
           >
             {isVisible ? (
               pokemonData.imagePath && (
-                <img
+                <ImageComponent
+                  height={isMobile ? '8rem' : '10rem'}
+                  width={isMobile ? '8rem' : '10rem'}
+                  imageSize={{
+                    width: isMobile ? 96 : 160,
+                    height: isMobile ? 96 : 160,
+                  }}
+                  densities={[1, 1.5]}
+                  alt={`pokemon_id_${pokemonData.pokemonNumber} ${pokemonData.name}`}
                   src={`${imageMode}/${pokemonData.imagePath}.webp`}
-                  alt={displayName}
-                  width={160}
-                  height={160}
-                  className="w-40 h-40 object-contain"
+                  sizes={isMobile ? '8rem' : '10rem'}
                   loading="lazy"
                   onLoad={handleImageLoad}
                   onError={handleImageError}
@@ -111,33 +130,40 @@ const ChampionsPokemonCard = ({
             return <TagComponent key={`${item}-id-${index}`} type={item} />
           })}
         </div>
+
         <dl
-          className="w-full grid grid-rows-[repeat(3,_1fr)] grid-cols-[35%_15%_35%_15%] mt-[0.55555556rem] px-[0.55555556rem]"
+          className="w-full max-w-[19rem] grid grid-rows-[repeat(3,_1fr)] grid-cols-[39%_13%_35%_13%] mt-4 mx-auto pl-2"
           aria-description="포켓몬 능력치 정보"
         >
-          <dt className="h-5 text-sm leading-5 even:ml-4">체력</dt>
-          <dd className="h-5 text-sm leading-5 text-right text-black">
-            {pokemonData.stats?.hp ?? '-'}
+          <dt className="h-5 text-[max(0.875rem,11px)] leading-5 mr-1">체력</dt>
+          <dd className="h-5 text-[max(0.875rem,11px)] leading-5 text-right text-black">
+            {pokemonData.stats?.hp}
           </dd>
-          <dt className="h-5 text-sm ml-4 leading-5 even:ml-4">공격</dt>
-          <dd className="h-5 text-sm leading-5 text-right text-black">
-            {pokemonData.stats?.attack ?? '-'}
+          <dt className="h-5 text-[max(0.875rem,11px)] leading-5 ml-2">공격</dt>
+          <dd className="h-5 text-[max(0.875rem,11px)] leading-5 text-right text-black">
+            {pokemonData.stats?.attack}
           </dd>
-          <dt className="h-5 text-sm leading-5 even:ml-4">특수공격</dt>
-          <dd className="h-5 text-sm leading-5 text-right text-black">
-            {pokemonData.stats?.specialAttack ?? '-'}
+          <dt className="h-5 text-[max(0.875rem,11px)] leading-5 mr-1">
+            특수공격
+          </dt>
+          <dd className="h-5 text-[max(0.875rem,11px)] leading-5 text-right text-black">
+            {pokemonData.stats?.specialAttack}
           </dd>
-          <dt className="h-5 text-sm ml-4 leading-5 even:ml-4">방어</dt>
-          <dd className="h-5 text-sm leading-5 text-right text-black">
-            {pokemonData.stats?.defense ?? '-'}
+          <dt className="h-5 text-[max(0.875rem,11px)] leading-5 ml-2">방어</dt>
+          <dd className="h-5 text-[max(0.875rem,11px)] leading-5 text-right text-black">
+            {pokemonData.stats?.defense}
           </dd>
-          <dt className="h-5 text-sm leading-5 even:ml-4">특수방어</dt>
-          <dd className="h-5 text-sm leading-5 text-right text-black">
-            {pokemonData.stats?.specialDefense ?? '-'}
+          <dt className="h-5 text-[max(0.875rem,11px)] leading-5 mr-1">
+            특수방어
+          </dt>
+          <dd className="h-5 text-[max(0.875rem,11px)] leading-5 text-right text-black">
+            {pokemonData.stats?.specialDefense}
           </dd>
-          <dt className="h-5 text-sm ml-4 leading-5 even:ml-4">스피드</dt>
-          <dd className="h-5 text-sm leading-5 text-right text-black">
-            {pokemonData.stats?.speed ?? '-'}
+          <dt className="h-5 text-[max(0.875rem,11px)] leading-5 ml-2">
+            스피드
+          </dt>
+          <dd className="h-5 text-[max(0.875rem,11px)] leading-5 text-right text-black">
+            {pokemonData.stats?.speed}
           </dd>
         </dl>
       </article>
